37ecb456e6ad5f0809e07122da30c5c5466bd8ae
Moekyawaung-mk <moekyawaung.mka2030@gmail.com>
2026-04-03 23:48:04 +0630

    Create sidebar.php

---
+++ includes/sidebar.php
@@ -0,0 +1,18 @@
+```php
+<aside class="col-md-2 sidebar bg-light border-end min-vh-100 p-3">
+  <h5 class="fw-bold mb-3">Menu</h5>
+  <div class="list-group">
+    <a href="/secure-pos-pro-ultimate-ui/index.php" class="list-group-item list-group-item-action">Dashboard</a>
+    <a href="/secure-pos-pro-ultimate-ui/products/index.php" class="list-group-item list-group-item-action">Products / ပစ္စည်းများ</a>
+    <a href="/secure-pos-pro-ultimate-ui/customers/index.php" class="list-group-item list-group-item-action">Customers / ဖောက်သည်များ</a>
+    <a href="/secure-pos-pro-ultimate-ui/suppliers/index.php" class="list-group-item list-group-item-action">Suppliers / ပံ့ပိုးသူများ</a>
+    <a href="/secure-pos-pro-ultimate-ui/stock/stock_in.php" class="list-group-item list-group-item-action">Stock In</a>
+    <a href="/secure-pos-pro-ultimate-ui/stock/stock_out.php" class="list-group-item list-group-item-action">Stock Out</a>
+    <a href="/secure-pos-pro-ultimate-ui/sales/checkout.php" class="list-group-item list-group-item-action">Checkout / အရောင်း</a>
+    <a href="/secure-pos-pro-ultimate-ui/sales/report.php" class="list-group-item list-group-item-action">Reports / အစီရင်ခံစာ</a>
+    <a href="/secure-pos-pro-ultimate-ui/sales/barcode.php" class="list-group-item list-group-item-action">Barcode Scanner</a>
+  </div>
+</aside>
+```
+
+---
